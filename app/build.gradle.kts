6da8561bb510b68292ff7e4138626eec9dde6f4c
DavideCapozzi <capozzi.davide99@gmail.com>
2026-01-01 18:36:55 +0100

    update: app version in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.antidoom.app"
         minSdk = 28 // Android 9 (Pie)
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
